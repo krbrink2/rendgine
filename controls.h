f42b963ece69a76fc2fe0f34e8b4c25be82c3c51
krbrink2 <krbrink2@illinois.edu>
2017-02-16 09:17:14 -0600

    Mesh seems all good

--- controls.h
+++ controls.h
@@ -5,18 +5,18 @@
 */
 
 // Anti-aliasing
-#define NUM_SAMPLES 		(1)
-#define SQRT_NUM_SAMPLES	(1)
+#define NUM_SAMPLES 		(9)
+#define SQRT_NUM_SAMPLES	(3)
 // Resolution
-#define	HRES 				(200)	// 1366,	1920
-#define VRES 				(200)	// 768,		1080
-#define S 					(.02)
+#define	HRES 				(500)	// 1366,	1920
+#define VRES 				(500)	// 768,		1080
+#define S 					(.002)
 // Camera
 #define	ORTHO				(false)
-#define EYE					Point3D(0, 0, 3)
-#define LOOKAT 				Point3D(0, 0, 0)
-#define UP 					Vector3D(0, 10, 0)
-#define	D 					(10)
+#define EYE					Point3D(0, .15, .8)
+#define LOOKAT 				Point3D(-.025, .065, 0)
+#define UP 					Vector3D(0, 1, 0)
+#define	D 					(3)
 
 // Good for perspec comparison for ortho: E 5, LOOKAT 0 0 -10, D 15
 
